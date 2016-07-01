91c9c69c39d047524b5192849b343bd69d8b4332
lijasminej <li.jasmine.j@gmail.com>
2016-07-01 01:37:45 -0400

    6. Routing - Add routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-import { HeroService } from './hero.service';
-import { HeroesComponent } from './heroes.component';
-
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HeroService }     from './hero.service';
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a [routerLink]="['/heroes']">Heroes</a>
+    <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
+  directives: [ROUTER_DIRECTIVES],
   providers: [
     HeroService
   ]
 })
-
 export class AppComponent {
   title = 'Tour of Heroes';
 }
