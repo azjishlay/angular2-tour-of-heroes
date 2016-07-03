59a55a329445985c459724f371a424e612534ec4
lijasminej <li.jasmine.j@gmail.com>
2016-07-02 22:12:52 -0400

    7. Http

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,18 +1,84 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'app/heroes';
+  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
   }
+
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve => setTimeout(() => resolve(HEROES), 2000)
     );
   }
+
   getHero(id: number) {
     return this.getHeroes().then(heroes => heroes.filter(hero => hero.id === id)[0]);
+  }
+
+  // Determine which method to call based on the state of the Hero
+  save(hero: Hero): Promise<Hero> {
+    if (hero.id) {
+      return this.put(hero);
+    }
+    return this.post(hero);
+  }
+
+  // Delete Hero
+  private delete(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+               .delete(url, headers)
+               .toPromise()
+               .catch(this.handleError);
+  }
+
+  // Update existing Hero
+  private put(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+               .put(url, JSON.stringify(hero), {headers: headers})
+               .toPromise()
+               .then(() => hero)
+               .catch(this.handleError);
+  }
+
+  // Add new Hero
+  private post(hero: Hero): Promise<Hero> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http
+               .post(this.heroesUrl, JSON.stringify(hero), {headers:headers})
+               .toPromise()
+               .then(res => res.json().data)
+               .catch(this.handleError);
+  }
+
+  // Error handling
+  private handleError(error: any) {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
   }
 }
